4e4ce904404fce8403b25672b7884e1e92611a47
aakragarwal <aakragarwal@microsoft.com>
2024-05-13 12:55:04 +0530

    Changes

--- scripts/bag.js
+++ scripts/bag.js
@@ -19,6 +19,14 @@ function displayBagSummary()
   let totalMRP = 0;
   let totalDiscount = 0;
 
+  if(totalItem == 0)
+    {
+      document.querySelector('.bag-page').innerHTML = `
+      <div class = "empty_bag">Your bag is empty!</div>
+      `
+      return;
+    }
+
   bagItemObjects.forEach(bagItem => {
 
     totalMRP += bagItem.original_price;
@@ -89,27 +97,28 @@ function removeFromBag(itemId)
 
 function generateItemHTML(item)
 {
-  return `<div class="bag-item-container">
-  <div class="item-left-part">
-    <img class="bag-item-img" src="../${item.image}">
+  return `
+  <div class="bag-item-container">
+    <div class="item-left-part">
+      <img class="bag-item-img" src="../${item.image}">
+    </div>
+    <div class="item-right-part">
+      <div class="company">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price-container">
+        <span class="current-price">${item.current_price}</span>
+        <span class="original-price">${item.original_price}</span>
+        <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <div class="return-period">
+        <span class="return-period-days">${item.return_period} days</span> return available
+      </div>
+      <div class="delivery-details">
+        Delivery by
+        <span class="delivery-details-days">${item.delivery_date}</span>
+      </div>
+    </div>
+    <div class="remove-from-cart" onclick = "removeFromBag(${item.id})">X</div>
   </div>
-  <div class="item-right-part">
-    <div class="company">${item.company}</div>
-    <div class="item-name">${item.item_name}</div>
-    <div class="price-container">
-      <span class="current-price">${item.current_price}</span>
-      <span class="original-price">${item.original_price}</span>
-      <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
-    </div>
-    <div class="return-period">
-      <span class="return-period-days">${item.return_period} days</span> return available
-    </div>
-    <div class="delivery-details">
-      Delivery by
-      <span class="delivery-details-days">${item.delivery_date}</span>
-    </div>
-  </div>
-
-  <div class="remove-from-cart" onclick = "removeFromBag(${item.id})">X</div>
-</div>`
+  `
 }
